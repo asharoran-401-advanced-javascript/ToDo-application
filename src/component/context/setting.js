c693266cf1dae33e31ad380f28a327058a39d7d8
asharaloran96 <asharoran96@gmail.com>
2020-03-07 23:35:15 +0200

    the app is broken

---
+++ src/component/context/setting.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line strict
+'use strict';
+
+import React, { useState } from 'react';
+export const SettingsContext = React.createContext(); // make a global function
+import cookie from 'react-cookies';
+
+const SettingProvider = (props) => {
+  const qs = new URLSearchParams(window.location.search); // constratur to create and render url search params as object
+  const cookieToken = cookie.load('auth');
+  const token = qs.get('token') || cookieToken || null;
+
+  const [displayCompleted , setDisplayCompleted] = useState(true);
+  const [countItem , setCountItem] = useState(5);
+  const [userToken , setUserToken] = useState(token);
+  const [login , setLogin] = useState(false);
+  const [logout , setLogout] = useState(true);
+  const [username , setUsername] = useState('');
+  const [password , setPassword] = useState('');
+
+  const state = { // pass all the state proprety and set proprwty in my state
+    displayCompleted,
+    countItem,
+    userToken,
+    login,
+    logout,
+    username,
+    password,
+    setDisplayCompleted,
+    setCountItem,
+    setUserToken,
+    setLogin,
+    setLogout,
+    setUsername,
+    setPassword,
+  };
+
+  return(
+    <SettingsContext.Provider value={state}>
+      {props.children}
+    </SettingsContext.Provider>
+  );
+};
+
+export default SettingProvider;
--- src/component/mainToDo/main.js
+++ src/component/mainToDo/main.js
@@ -1,25 +1,29 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line strict
 'use strict';
-
 import React , {useEffect , useState, useContext } from 'react';
+import { SettingsContext } from '../context/setting.js';
 import uuid from 'uuid/v4';
 import { When , If , Then , Else } from '../if/if.js';
 import Modal from '../model/model.js';
 import '../mainToDo/main.scss';
-import  SettingsContext  from '../../context/setting.js';
+import Auth from '../auth/auth.js';
+import cookie from 'react-cookies';
+import superagent from 'superagent';
 
 const ToDo = props => {
 
-  let settingContext = useContext(SettingsContext);
+  const settingContext = useContext(SettingsContext);
 
-  // because we use finction here because that we use (useContext)
+  // because we use function here because that we use (useContext)
   //   //----------------------------------Hooks ----------------------------------------//
   const [todoList , setTodoList] = useState([]); // arr
   const [item , setItem] = useState({}); // i pass is as a object beause it's easy to use object proparty
   const [details , setDetails] = useState({}); //because each item have details
   const [showDetails , setShowDetails] = useState(false); // as initial value hide the details (I use this state in the toggle to show/hide)
 
+
+  const API = process.env.REACT_APP_API;
   useEffect(() => { // similar to componentDidMount and componentDidUpadate
     let complete = todoList.filter(item => !item.complete).length; // true
     let incomplete = todoList.filter(item => item.complete).length; // false
@@ -78,32 +82,80 @@ const ToDo = props => {
       settingContext.setDisplayCompleted(true);
     }
   };
+  const handleLoginSubmit = (e) => { // this method we use it on submit the form of user info so we hite the APi and pass user info on it
+    e.preventDefault();
+    superagent //use Promise
+      .post(`${API}/signin`)
+      .auth(settingContext.username , settingContext.password)
+      .set('mode' , 'cors')
+      .then( result => {
+        let token = result.text;
+        login(token);
+      })
+      .catch(e => console.error(e));
+  };
+  const login = (token) => {
+    cookie.save('auth' , token);
+    settingContext.setUserToken(token);
+    settingContext.setLogin(true);
+  };
+
+  const logout = () => {
+    cookie.remove('auth');
+    settingContext.setUserToken();
+    settingContext.setLogin(false);
+  };
+
+  const handleLoginChange = (e) => {
+    if(e.target.name === 'username'){
+      settingContext.setUsername(e.target.value);
+    }
+    if(e.target.name === 'password'){
+      settingContext.setPassword(e.target.value);
+    }
+  };
+
 
   return (
     <>
+      <section className='login-form'>
+        <If condition={settingContext.login}>
+          <Then>
+            <button onClick={logout}>LogOut</button>
+          </Then>
+          <Else>
+            <form onSubmit={handleLoginSubmit}>
+              <input name='username' placeholder='Enter username' onChange={handleLoginChange}/>
+              <input name='password' placeholder='Enter your password please' onChange={handleLoginChange}/>
+              <input name='submit' type='submit' value='login'/>
+            </form>
+          </Else>
+        </If>
+      </section>
       <section className="todo">
-        <div>
-          <form onSubmit={addItem}>
-            <label>
-              <span>ToDo Item :</span>
-              <input name="text" placeholder="Add Item" onChange={handleInputChange}/>
-            </label>
-            <label>
-              <span>Difficulty :</span>
-              <input type="range" min="1" max="5" name="difficulty" defaultValue="3" className="difficulty" onChange={handleInputChange} />
-            </label>
-            <label>
-              <span>Assigned To :</span>
-              <input  type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
-            </label>
-            <label>
-              <span>Date :</span>
-              <input type="date" name="Time" onChange={handleInputChange} />
-            </label>
-            <button className="form-button">Add Item</button>
-          </form>
-        </div>
-
+        <Auth capability='create'>
+          <div>
+            <form onSubmit={addItem}>
+              <label>
+                <span>ToDo Item :</span>
+                <input name="text" placeholder="Add Item" onChange={handleInputChange}/>
+              </label>
+              <label>
+                <span>Difficulty :</span>
+                <input type="range" min="1" max="5" name="difficulty" defaultValue="3" className="difficulty" onChange={handleInputChange} />
+              </label>
+              <label>
+                <span>Assigned To :</span>
+                <input  type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
+              </label>
+              <label>
+                <span>Date :</span>
+                <input type="date" name="Time" onChange={handleInputChange} />
+              </label>
+              <button className="form-button">Add Item</button>
+            </form>
+          </div>
+        </Auth>
         <div>
           <ul>
             {todoList.map(item => (
@@ -115,29 +167,35 @@ const ToDo = props => {
                   {item.text}
                 </span>
                 <button onClick={() => toggleDetails(item.key)}>more Details</button>
-                <button onClick={() => deleteItem(item.key)}>Delete</button>
+                <Auth capability="delete">
+                  <button onClick={() => deleteItem(item.key)}>
+                            Delete
+                  </button>
+                </Auth>
               </li>
             ))}
           </ul>
         </div>
-        <div>
-          <h2>
+        <Auth capability='read'>
+          <div>
+            <h2>
             There are [ {todoList.filter( item => !item.complete).length}
             ] Item to complete
-          </h2>
-          {/* <If condition={!settingContext}> */}
-          <Then>
-            <button className="display-button" name="hideCompleteButton" onClick={toggleHideComleted}>
+            </h2>
+            {/* <If condition={!settingContext}> */}
+            <Then>
+              <button className="display-button" name="hideCompleteButton" onClick={toggleHideComleted}>
               Display Completed
-            </button>
-          </Then>
-          <Else condition={settingContext}>
-            <button className="display-button" name="hideCompleteButton" onClick={toggleHideComleted}>
+              </button>
+            </Then>
+            <Else condition={settingContext}>
+              <button className="display-button" name="hideCompleteButton" onClick={toggleHideComleted}>
                Display All
-            </button>
-          </Else>
-          {/* </If> */}
-        </div>
+              </button>
+            </Else>
+            {/* </If> */}
+          </div>
+        </Auth>
       </section>
       {/* i need to show the details by condition because that i use if  */}
       <When condition={showDetails}>
